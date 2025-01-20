e4bae7a4184211003a17059ef8d6c98ceec3e2e0
PanagiotisDerekas <139541822+PanagiotisDerekas@users.noreply.github.com>
2025-01-20 13:58:42 +0200

    Update AKU-net.py

--- Actinic Keratosis/AKU-net.py
+++ Actinic Keratosis/AKU-net.py
@@ -1,8 +1,9 @@
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, BatchNormalization, Activation, Reshape, concatenate
 from tensorflow.keras.models import Model
+import numpy as np
 
-def AKU_net(input_size=(256,256,3), dropout_rate=0.6):
+def Unet(input_size=(256,256,3), dropout_rate=0.6):
     # Load the VGG-16 model and its pre-trained weights
     vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=input_size)
     vgg16_layers = dict([(layer.name, layer) for layer in vgg16.layers])
@@ -30,15 +31,19 @@ def AKU_net(input_size=(256,256,3), dropout_rate=0.6):
     conv4 = vgg16_layers['block4_conv1'](pool3)
     conv4 = vgg16_layers['block4_conv2'](conv4)
     conv4 = vgg16_layers['block4_conv3'](conv4)
-    drop5 = Dropout(dropout_rate)(conv4)
+    drop4 = Dropout(dropout_rate)(conv4)
+
+
+    conv5 = vgg16_layers['block5_conv1'](drop4)
+    conv5 = vgg16_layers['block5_conv2'](conv5)
+    conv5 = vgg16_layers['block5_conv3'](conv5)
+    drop5 = Dropout(dropout_rate)(conv5)
+
     up6 = Conv2DTranspose(256, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(drop5)
     up6 = BatchNormalization(axis=3)(up6)
     up6 = Activation('relu')(up6)
 
-    x1 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(drop3)
-    x2 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(up6)
-    merge6  = concatenate([x1,x2], axis = 1)
-    merge6 = ConvLSTM2D(filters = 256, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+    merge6  = concatenate([drop3, up6], axis = 3)
 
     conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
     conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
@@ -47,10 +52,7 @@ def AKU_net(input_size=(256,256,3), dropout_rate=0.6):
     up7 = BatchNormalization(axis=3)(up7)
     up7 = Activation('relu')(up7)
 
-    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
-    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up7)
-    merge7  = concatenate([x1,x2], axis = 1)
-    merge7 = ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge7)
+    merge7  = concatenate([conv2, up7], axis = 3)
 
     conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
     conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
@@ -59,10 +61,7 @@ def AKU_net(input_size=(256,256,3), dropout_rate=0.6):
     up8 = BatchNormalization(axis=3)(up8)
     up8 = Activation('relu')(up8)
 
-    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
-    x2 = Reshape(target_shape=(1, N, N, 64))(up8)
-    merge8  = concatenate([x1,x2], axis = 1)
-    merge8 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge8)
+    merge8  = concatenate([conv1, up8], axis = 3)
 
     conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
     conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
